d3ac4c4eb6511b0528e833a77849ec7c4e5444d8
hemanth-kdex <hemanth@kdexglobal.com>
2025-03-22 23:49:27 -0500

    Updates

---
+++ _next/static/cb-v_FUrYESnLIE5AK8OO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-1918437d7d007f81.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
